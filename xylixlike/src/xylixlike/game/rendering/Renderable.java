07027226b488a6859a92dbe1818e23a4e5ff1d1a
Kerkko Pelttari <kerk.pelt@gmail.com>
2019-03-29 21:01:21 +0200

    Add first test file.
    Implement renderable

---
+++ xylixlike/src/xylixlike/game/rendering/Renderable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xylixlike.game.rendering;
+
+/**
+ *
+ * @author xylix
+ */
+public interface Renderable {
+    String row(int i);
+    String column(int i);
+}
---
+++ xylixlike/src/xylixlike/game/rendering/VisualStatus.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xylixlike.game.rendering;
+
+import java.util.HashMap;
+
+
+/**
+ *
+ * @author xylix
+ */
+public class VisualStatus implements Renderable{
+    private final HashMap<Integer, StringBuilder> yAxis;
+    
+    public VisualStatus(int height, int width) {
+        yAxis = new HashMap<>();
+        for(int i = 0; i < height; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < width; j++)
+                sb.append(" ");
+            yAxis.put(i, (sb));
+        }
+    }
+    
+    @Override
+    public String row(int i) {
+        return yAxis.get(i).toString();
+    }
+    
+    @Override
+    public String column(int i) {
+        StringBuilder column = new StringBuilder();
+        yAxis.forEach((k, v) -> column.append(v.charAt(i)));
+        return column.toString();
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder toString = new StringBuilder();
+        yAxis.forEach((k, v) -> toString.append(v).append("\n"));
+        return toString.toString();
+    }
+    
+    public void place(Character c, int xCoord, int yCoord) {
+        yAxis.get(xCoord).replace(xCoord, xCoord +1, c.toString());
+    }
+}
